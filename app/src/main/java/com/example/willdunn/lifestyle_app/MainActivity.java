f7cfea738421a9798618342665e3198c3ec23646
Will Dunn <dunn_powell@yahoo.com>
2019-08-23 15:09:06 -0600

    added location backend activity with minor xml for where it will display

--- app/src/main/java/com/example/willdunn/lifestyle_app/MainActivity.java
+++ app/src/main/java/com/example/willdunn/lifestyle_app/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.willdunn.lifestyle_app;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
+    private String userLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        client = LocationServices.getFusedLocationProviderClient(this); //grab lat/lon from user
+        Button btn = findViewById(R.id.btn_update_Profile);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                switch(v.getId()) {
+
+                    case R.id.btn_update_Profile: {
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if(location != null){
+                                    userLocation = location.toString(); //TODO this might need to be changed if it grabs tons of info
+                                } else {
+                                    Toast.makeText(MainActivity.this,"Could not get your location",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                        //todo error handle the rest of the fields (aside from location)
+
+
+                        //todo propagate the fields of the other texts
+
+
+                        break;
+                    }
+                    /*
+                    //todo create the passage of location when the correct module is selected
+                    case R.id.btn_module_Hikes: {
+
+                        //Start an activity and pass the location string to it.
+                        Intent locIntent = new Intent(this, MapsActivity.class);
+                        locIntent.putExtra("BTN_STRING_LOC", userLocation);
+                        this.startActivity(locIntent); //this is not needed here, but is helpful for readability
+
+                        break;
+                    }
+                     */
+                }
+            }
+        });
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{ ACCESS_FINE_LOCATION}, 1);
     }
 }
---
+++ app/src/main/java/com/example/willdunn/lifestyle_app/MapsActivity.java
@@ -0,0 +1,57 @@
+package com.example.willdunn.lifestyle_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+
+public class MapsActivity extends AppCompatActivity {
+
+    private String userLocation = "geo:40.767778,-111.845205?q="; //TODO: change this to be the passed in the location, not default
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Get the intent that created this activity.
+        Intent receivedIntent = getIntent();
+
+        //Get the string data of the location
+        userLocation = receivedIntent.getStringExtra("BTN_STRING_LOC");
+
+        if(userLocation.isEmpty()){
+            Toast.makeText(MapsActivity.this, "No location received", Toast.LENGTH_SHORT).show();
+        } else {
+            //We have to search for nearby hikes and construct a URI object from it.
+            Uri searchUri = Uri.parse(userLocation + "hikes"); //hikes is a "magic number"
+
+            //Create the implicit intent
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, searchUri);
+
+            //If there's an activity associated with this intent, launch it
+            if(mapIntent.resolveActivity(getPackageManager())!=null){
+                startActivity(mapIntent);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
